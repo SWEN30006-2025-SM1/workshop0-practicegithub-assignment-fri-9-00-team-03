620f9be5df7dda7e0ab653480aed7d9a934beff6
bkimwoern <beatrice.kimwoern@student.unimelb.edu.au>
2025-03-07 10:27:49 +1100

    Fixed calculator, add throw error for division by 0

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,21 +3,33 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 4;
+        int sum;
+        sum = a + b;
+        return sum;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        int difference;
+        difference = a - b;
+        return difference;
     }
 
     public int multiply(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        int product;
+        product = a * b;
+        return product;
     }
 
     public double divide(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        int quotient;
+        if (a == 0){
+            System.out.println("Division by zero not allowed");
+            return 0;
+        }
+        quotient = a/b;
+        return quotient;
     }
 }
