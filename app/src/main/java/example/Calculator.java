d0b8e1a0a01a6babf903cb14db9ebf59108d5c2b
oelechi <obiorranicolas.elechi@student.unimelb.edu.au>
2025-03-07 10:30:39 +1100

    FIX: fixed hardcoded calcultator issues.

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,22 +2,32 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
-        return 4;
+        int sum;
+        sum = a + b;
+        return sum;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        int difference;
+        difference = a - b;
+        return difference;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        int product;
+        product = a * b;
+        return product;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
-        return 5;
+        int quotient;
+
+        if (a/b == 0) {
+            System.out.println("error: divide by zero error");
+            return 0;
+        } else {
+            quotient = a/b;
+            return quotient;
+        }
     }
 }
