4dda3aa78df5883e707bbf65a240a208df3e0c0c
bkimwoern <beatrice.kimwoern@student.unimelb.edu.au>
2025-03-14 09:21:52 +1100

    just testing after redownloading

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -2,6 +2,7 @@ package example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hi just testing\n");
         Calculator calculator = new Calculator();
         System.out.println("add = " + calculator.add(3, 5));
         System.out.println("minus = " + calculator.minus(8, 2));
